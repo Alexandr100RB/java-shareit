e563a0eeb97791b15d2cb5ca79b7eae5ca811092
Alexander Kosyanin <kosyanin@yandex.ru>
2025-06-20 02:05:11 +0300

    исправлены ошибки

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -23,7 +23,7 @@ import static java.util.stream.Collectors.toList;
 @RestController
 @RequestMapping("/items")
 public class ItemController {
-    private final String OWNER = "X-Sharer-User-Id";
+    private static final String OWNER = "X-Sharer-User-Id";
     private ItemService itemService;
     private ItemMapper mapper;
     private CheckConsistencyService checker;
